81f38c43553bbf19fcd5b6bd978b32d7a97b463b
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2022-09-27 19:01:12 +0530

    Create CurrentAccount.java

---
+++ CurrentAccount.java
@@ -0,0 +1,15 @@
+public class CurrentAccount implements Account{
+
+    int balance;
+    int withdrawals;
+
+    @Override
+    public void display_balance() {
+
+    }
+
+    @Override
+    public void display_transaction_history() {
+
+    }
+}
