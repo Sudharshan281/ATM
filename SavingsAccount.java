44edfa139012113495bde6493674c326cbd617f9
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2022-09-27 19:00:54 +0530

    Create SavingsAccount.java

---
+++ SavingsAccount.java
@@ -0,0 +1,13 @@
+public class SavingsAccount implements Account{
+
+    int balance;
+    @Override
+    public void display_balance() {
+
+    }
+
+    @Override
+    public void display_transaction_history() {
+
+    }
+}
