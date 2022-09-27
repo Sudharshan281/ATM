b2b09235144b400c345f65341e3758ef2342e371
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2022-09-27 22:23:43 +0530

    Update CurrentAccount.java

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -3,6 +3,11 @@ public class CurrentAccount implements Account{
     int balance;
     int withdrawals;
 
+    public CurrentAccount() {
+        this.balance = 0;
+        this.withdrawals = 0;
+    }
+
     @Override
     public void display_balance() {
 
@@ -11,5 +16,26 @@ public class CurrentAccount implements Account{
     @Override
     public void display_transaction_history() {
 
+    }
+
+    @Override
+    public void deposit(int amt) {
+        this.balance += amt;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void changeBalance(int add) {
+        this.balance += add;
+    }
+
+    public int getWithdrawals() {
+        return withdrawals;
+    }
+
+    public void setWithdrawals(int withdrawals) {
+        this.withdrawals = withdrawals;
     }
 }
