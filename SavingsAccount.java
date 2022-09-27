322d93b0668c13b670bf406d53c0ff4071ba8a7c
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2022-09-27 22:23:24 +0530

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,6 +1,31 @@
+import java.util.Date;
+
 public class SavingsAccount implements Account{
 
     int balance;
+    Date entry;
+
+    public SavingsAccount() {
+        this.balance = 0;
+        this.entry = new Date();
+    }
+
+    protected Date getEntry() {
+        return entry;
+    }
+
+    protected void setEntry(Date entry) {
+        this.entry = entry;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void changeBalance(int add) {
+        this.balance += add;
+    }
+
     @Override
     public void display_balance() {
 
@@ -9,5 +34,10 @@ public class SavingsAccount implements Account{
     @Override
     public void display_transaction_history() {
 
+    }
+
+    @Override
+    public void deposit(int amt) {
+        this.balance += amt;
     }
 }
