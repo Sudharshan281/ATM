7ca0e76e07842f6e3572e42bd25c957545ff9684
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2022-10-01 17:07:24 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,9 +1,11 @@
 public class User {
     /**
-     * User has id, password, savings account, current account
+     * This class stores all the information about the user
+     * In my implementation of atm, each user has a unique id and a password, a savings account
+     * and a current account
      */
 
-    private final int id;
+    private final int id;       //id is final because it can't be changed
     private int password;
     SavingsAccount savings_acc;
     CurrentAccount cur_acc;
@@ -14,6 +16,10 @@ public class User {
         this.savings_acc = new SavingsAccount() ;
         this.cur_acc = new CurrentAccount();
     }
+
+    /**
+     * below are the required getters and setters for this class
+     */
 
     public int getId() {
         return id;
