6427e0605fe28493c979368a97c5aeba613d7182
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2022-10-01 17:06:33 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,7 +1,31 @@
 public interface Account {
+
+    /**
+     * This interface has the common methods required for an account
+     * and both savings account and current account implements this interface
+     */
+
+    /**
+     *
+     * @return balance amount of the respective accounts
+     */
     int getBalance();
+
+    /**
+     *
+     * @param amt : gets amt and adds it to the balance of the account
+     */
     void deposit(int amt);
-    void changeBalance(int amt);
+
+    /**
+     * @param amt
+     * @return false if amt is greater than balance, or true, and it withdraws the amount
+     */
     boolean withdraw(int amt);
+
+    /**
+     * @param amt : changed by balance by amount value
+     */
+    void changeBalance(int amt);
 
 }
